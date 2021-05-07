5e980831e7988e3746a3c00c33f9e9e20be917b3
Sainath <Sainath.katakam@gmail.com>
2021-05-07 09:08:48 +0530

    deleted unneccessary pom.xml

--- spring-data-jpa-demo/src/main/java/com/javatechie/springdatajpademo/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javatechie.springdatajpademo.repository;
-
-import com.javatechie.springdatajpademo.entity.Customer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer,Integer> {
-    
-}
--- spring-data-jpa-demo/src/main/java/com/javatechie/springdatajpademo/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javatechie.springdatajpademo.repository;
-
-import com.javatechie.springdatajpademo.entity.Product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Integer> {
-    
-}
